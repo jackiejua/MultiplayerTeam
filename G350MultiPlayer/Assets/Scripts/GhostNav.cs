9ed2c9e62da44187142b38f621f00a01ac15b241
jackiejua <119469167+jackiejua@users.noreply.github.com>
2024-11-21 09:58:58 -0700

    I FIXED IT

--- G350MultiPlayer/Assets/Scripts/GhostNav.cs
+++ G350MultiPlayer/Assets/Scripts/GhostNav.cs
@@ -23,7 +23,7 @@ public class GhostNav : MonoBehaviour
     
     void Update()
     {
-        if(agent.remainingDistance <= agent.stoppingDistance) //done with path
+      /*  if(agent.remainingDistance <= agent.stoppingDistance) //done with path
         {
             Vector3 point;
             if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
@@ -32,6 +32,7 @@ public class GhostNav : MonoBehaviour
                 agent.SetDestination(point);
             }
         }
+        */
 
     }
     bool RandomPoint(Vector3 center, float range, out Vector3 result)
@@ -52,11 +53,13 @@ public class GhostNav : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene("Died");
+            Debug.Log("Player has been caught");
         }
     }
 }
